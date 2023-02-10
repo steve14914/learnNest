081ae0c9c9540680c7ab69c5d4ffc3a60c65ba70
steve14914 <noh6333@gmail.com>
2023-02-10 19:49:28 +0900

    trying Post, Param, Body

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 
 @Controller('movie')
 export class MovieController {
@@ -6,6 +6,20 @@ export class MovieController {
     @Get()
     getDefault() {
         return 'asdf';
+    }
+
+    @Get(':id')
+    getID(@Param('id') gettingID: string) {
+        return `the ID you are looking for is : ${gettingID}`;
+    }
+
+    @Post(':id')
+    getMovieInfo(@Param('id') gettingID: string, @Body() movieInfo) {
+        return {
+            id: gettingID,
+            ...movieInfo,
+        }
+
     }
 
 }
